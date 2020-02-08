867f0c5348109636a97de9f3e650726bf50889de
KateProtasevich <KrishniovaKate@mail.ru>
2020-02-09 00:51:51 +0300

    feat: add start page and 4 pages

---
+++ Treasure_hunters/images/png-fixer2.js
@@ -0,0 +1,12 @@
+function pngInit(){
+try {
+	document.execCommand("BackgroundImageCache", false, true); /* TredoSoft Multiple IE doesn't like this, so try{} it */
+} catch(r) {}
+DD_belatedPNG.createVmlNameSpace();
+DD_belatedPNG.createVmlStyleSheet();
+}
+function png(){
+
+  DD_belatedPNG.fix('.leaves');
+}
+png();
